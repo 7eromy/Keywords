464e9dcf4de6ae7dfeb5c7499562b63ce1e5675b
7eromy <147342137+7eromy@users.noreply.github.com>
2024-12-24 12:43:04 +0300

    Update Keywords.py

--- Keywords.py
+++ Keywords.py
@@ -15,17 +15,7 @@ dp = Dispatcher()
 
 # Учетные данные для работы gspread
 credentials = {
-    "type": "service_account",
-  "project_id": "keywords-445708",
-  "private_key_id": "ceef495c0325c91fc457ffa8eddebff1708d7d6e",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC046UEFZ+MyYqC\nA2pwcLcBjKmT0zXerUag/0R/8MJxsoj0NVn5yMVkybPTX7x/91CLMx7HXXzvqjeM\nI++C0SiZDb5mPphpIvvlPOrvCTMfp/EjJkZeAtCa6nrLGVojEdGOdKv6T8NPecxF\ntcG0ZUTyj+B52EXIm283L9tiA3MvhcIUho3ciOn4O992oPV02QJ+H1ISs4NKyAQf\nDriTB0pZv8nhUj5H+5BSo7AzISuuw/K7y7MbFdHJaHsabR6Z+b21Cvpt9sC366C/\nY+Md+1gJnZenrZS82/zOytzexAz5fPJUS7EcGvV3vCXFeUHMnZKTGeOjkehADnVg\nhL6I6VpBAgMBAAECggEAGb1I1l9zUBe/l0eAJlbLm7NVz/yebjyuz6rzJtdt+7rB\nZSqQToxEllZjOcEmM/lPozcXepvxcrMAa3cTlkRH6Bt+C9N9YXyCZni4H2JqPWdq\n1ysTmT4CTADwjyTg4Buhe4lbjWc7LfxxVjwKP66nVHKnqP7e0zBW733mVDiUc97x\nVYFBw7s69vOm12lJdwz9uHkjLdP+mjxbs7w1nXSp3Mand0M/miRHokz8gu2r0aAx\nG1z4VFJv4hw0PTgbb2T16600IuM2Zeh68mEWyzPl9jrsCx34wzI7Do4IhZmO3xks\n8eF+FImPf1hMFyaHn1cVL/D7EPPBzztgOJB+u+8U1wKBgQD2ShYtLQ2+WzS4YMD/\nUYN8x2gYmAXAPHcHIgA3xGRwRCzxmjsGIi2FEK14FXcyYIywPLXr/vyCAl5z7V6X\n5LCK4rHaHrRfX2aMM8JO1FFteVsmt+gFr8Bj0OhpKLl0k1xKICp4iSb+0qq4QTx/\ntcYxl1gzQKGA69vZ7uoenJH/1wKBgQC8BXGXhTgyYA4B7Y8Ck/ujZ1Rd+iG/NNUB\nNqcOTU3P0avt0NZFxEXEQHnh3YEpBaItaX4FHWxHSyzTu9m7hmbE3Eg/pKNRWhaC\nV43yVDavAPObOYiloCOJtQMFby/D/OB11BCIDzHyv8/D7APNY54rPwkvwATD+neN\nn57mIfWTpwKBgGWvPOy190Cqg1/EgqpnSzRPWAkfavBthm8peJGNwjIf07aEiO4F\nWmnf8t7rbeUcu7lft3SSOEqtE7YlQLLRtpoA8pEtsCbYSoyEKitFucDLI/keYOtI\nHtSXQf141OjulmH8WcyeQ71d2SJtvL61m/iJld7DRmrl9kVKQNsWpajNAoGAeiRX\nLIVggmkf8GSwuI4FtJsVlY8+iQX7MbSdRY82c0DUHcPCzsO4RstT4kSQ+WMtfa7A\nLPZ2NsJBNrMbuNSQwmYPkTiU1+5cHQRftAK9G2bU2gvAF0g2jejHM6qNLxaBgknn\nC1xDqFyuzanutmh2gcWLZDerYNy++YOuJ/X7oJUCgYAXtwv3z/U9NkyN+ztlHAvY\nQcoCtfOxf3fmD3bZUAvG43qKGBarZLzI59ZuLH/tJgMiPEzWg88cmCSSYkMYHpe+\nl38WywYPmjHy8W0rkykB5qYb2KxbY5g7ByEBg6hJD6m/OFXEoqwvuhAKfbiu3X1Z\nqF3b15RHfPS+J6fuhdOMJw==\n-----END PRIVATE KEY-----\n",
-  "client_email": "keywords@keywords-445708.iam.gserviceaccount.com",
-  "client_id": "112421599867897000722",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/keywords%40keywords-445708.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
+  # Здесь необходимо использовать учетные данные Google Cloud
 }
 
 # Функция для получения ключевых слов из Google таблицы
